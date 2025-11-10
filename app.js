0df1b2f5888736e91c8478cca024d74ba8791f76
RafaaDomingues <rafaellycaroline199@gmail.com>
2025-11-10 11:49:46 -0300

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('back-to-top-button');
 
 const ChegarAoTopo = () => {
-    const scrollDuration = 200; // Deixar mais suave
+    const scrollDuration = 100; // Deixar mais suave
     const scrollStep = -window.scrollY / (scrollDuration / 15);
     
     const scrollInterval = setInterval(() => {
